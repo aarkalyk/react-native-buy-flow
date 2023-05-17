3ab43ff1f94b98601563980eac98034779ac14d3
Arkalyk Akash <arkalyk.akash@nu.edu.kz>
2023-05-17 10:24:13 +0200

    Increase back button hitSlop so that it's easier to press it (#4)
    Co-authored-by: Arkalyk Akash <a.arkalyk@gmail.com>

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -18,7 +18,11 @@ export const NavBar = ({ children, onPressBack }: Props) => {
 
 const BackButton = ({ onPress }: { onPress(): void }) => {
   return (
-    <TouchableOpacity accessibilityLabel="Back button" onPress={onPress}>
+    <TouchableOpacity
+      hitSlop={20}
+      accessibilityLabel="Back button"
+      onPress={onPress}
+    >
       <Svg.Svg width="24pt" height="24pt" viewBox="0 0 700 700">
         <Svg.Path
           fill="black"
